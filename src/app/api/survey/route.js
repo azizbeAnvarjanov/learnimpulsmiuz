00c53b4e53ac2c994b87f3f840942385c098ee32
azizbeAnvarjanov <azizbeanvarjanov@gmail.com>
2025-06-10 18:18:46 +0500

    add sirovnoma 2

--- src/app/api/survey/route.js
+++ src/app/api/survey/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "@/app/supabaseClient";
 
 export async function POST(request) {
   const body = await request.json();
